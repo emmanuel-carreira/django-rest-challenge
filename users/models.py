d9ad53df7dcdb685a1634545c8923643feb16dbd
Emmanuel Carreira <emmanuel_carreira@hotmail.com>
2021-01-27 22:15:28 -0300

    feat: adds initial Profile and Phone models

---
+++ users/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField()
+
+    class Meta:
+        ordering = ['email']
+
+    def __str_(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Phone(models.Model):
+    number = models.PositiveIntegerField()
+    area_code = models.PositiveSmallIntegerField()
+    country_code = models.CharField(max_length=4)
+    phone = models.ForeignKey(Profile, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f'{self.country_code} {self.area_code} {self.number}'
