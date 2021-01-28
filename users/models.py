f0f22b909467b2bf9d2cabdba24b2c8ed37d94fd
Emmanuel Alves <emmanuelalves@locus.software>
2021-01-28 09:44:06 -0300

    refactor: adds unique constraint to model field
    1. Adds unique constraint to email field
    
    2. Adds aut_now_add to last_login field
    
    3. Fixes typo in str dunder method
    
    4. Removes unused User model import

--- users/models.py
+++ users/models.py
@@ -1,19 +1,18 @@
-from django.contrib.auth.models import User
 from django.db import models
 
 
 class Profile(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-    last_login = models.DateTimeField()
+    last_login = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['email']
 
-    def __str_(self):
+    def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
 
