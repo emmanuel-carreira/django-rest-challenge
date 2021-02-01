23adf762d6161d131f336c625a06a210e35ae986
Emmanuel Alves <emmanuelalves@locus.software>
2021-02-01 11:16:34 -0300

    Revert "fix: fixes get_token_for_user() return decodification"
    This reverts commit 3ba8c5544274951fa3dd4190d4f43688d5609cd4.

--- users/utils.py
+++ users/utils.py
@@ -3,4 +3,4 @@ from rest_framework_simplejwt.tokens import AccessToken
 
 def get_token_for_user(user):
     access_token = AccessToken.for_user(user)
-    return access_token
+    return str(access_token)
