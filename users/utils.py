3ba8c5544274951fa3dd4190d4f43688d5609cd4
Emmanuel Alves <emmanuelalves@locus.software>
2021-02-01 11:01:01 -0300

    fix: fixes get_token_for_user() return decodification

--- users/utils.py
+++ users/utils.py
@@ -3,4 +3,4 @@ from rest_framework_simplejwt.tokens import AccessToken
 
 def get_token_for_user(user):
     access_token = AccessToken.for_user(user)
-    return str(access_token)
+    return access_token
